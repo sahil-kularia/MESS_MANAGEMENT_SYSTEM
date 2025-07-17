17ac4c9fc8e162308daa9e0dde6e861fe2d66c62
sahil-kularia <sahilkularia15@gmail.com>
2025-07-17 15:42:08 +0530

    nwe one

--- src/dashboard/Ngo.jsx
+++ src/dashboard/Ngo.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL;
+// const API_BASE = import.meta.env.VITE_API_URL;
 
 export default function Ngo() {
   const [ngoForm, setNgoForm] = useState({
